9ee155e72df9b9a6c525b231087d63f9bb87fd34
Yesenia19 <1719110284@utectulancingo.edu.mx>
2022-06-20 16:32:06 +0000

    actualizar_clientes

--- api_rest/code/main.py
+++ api_rest/code/main.py
@@ -44,8 +44,8 @@ async def clientes(id: int):
         response = cursor.fetchall()
         return response
 
-@app.put("/clientes/", response_model=Respuesta)
-async def clientes(nombre: str, email:str):
+@app.post("/clientes/", response_model=Respuesta)
+async def clientes():
     #conexion a una bd cierra automaticamente el archivo que se utilice
     with sqlite3.connect('sql/clientes.sqlite') as connection:
         connection.row_factory=sqlite3.Row
@@ -54,6 +54,18 @@ async def clientes(nombre: str, email:str):
         cursor.execute("INSERT INTO clientes (nombre, email) values ('{}','{}');".format(nombre,email))
         #ordena los formatos en json
         response = {"mensaje":"Cliente agregado"}
+        return  response
+
+@app.put("/clientes/", response_model=Respuesta)
+async def clientes(id:int, nombre: str, email:str):
+    #conexion a una bd cierra automaticamente el archivo que se utilice
+    with sqlite3.connect('sql/clientes.sqlite') as connection:
+        connection.row_factory=sqlite3.Row
+        #cursor para realizar las operaciones en la BD
+        cursor = connection.cursor()
+        cursor.execute("UPDATE clientes SET nombre='{}', email='{}' WHERE id_cliente={} ;".format(nombre,email,id))
+        #ordena los formatos en json
+        response = {"mensaje":"Cliente actualizado"}
         return  response
         
 
